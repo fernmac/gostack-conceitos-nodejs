9f4eea07d911fa8fa12eb0d88cc95838262cff59
Fernando Veras Machado <fernando.veras@live.com>
2020-06-17 16:58:53 -0300

    Adicionando as funcionalidades do back-end

---
+++ README.md
@@ -0,0 +1,47 @@
+# GOSTACK - Conceitos do Node JS
+
+![](https://img.shields.io/badge/made%20by-fernmac-04d361?style=flat&color=04d361) 
+![](https://img.shields.io/github/languages/count/fernmac/gostack-conceitos-nodejs?style=flat&color=04d361) 
+[![GitHub stars](https://img.shields.io/github/stars/fernmac/gostack-conceitos-nodejs?style=social)](https://github.com/fernmac/gostack-conceitos-nodejs/stargazers)
+
+Aplicação para armazenar repositórios, que irá permitir a criação, listagem, atualização e remoção dos repositórios, e além disso permitir que os repositórios possam receber "likes".
+
+------------
+
+## Instalação
+
+- Para iniciar a instalação é necessário clonar o repositório
+    >`git clone [url_repositorio]`
+
+- Entrar no diretório clonado
+    >`cd gostack-conceitos-nodejs`
+
+- Instalar as dependências do projeto via **NPM** ou **YARN**
+    >`npm install`
+
+## Execução
+
+- Executar o comando para iniciar o back-end
+    >`npm run dev`
+
+## Rotas
+
+### **`POST`** `/repositories`
+
+A rota deve receber title, url e techs dentro do corpo da requisição, sendo a URL o link para o github desse repositório. Ao cadastrar um novo projeto, ele deve ser armazenado dentro de um objeto no seguinte formato: { id: "uuid", title: 'Desafio Node.js', url: 'http://github.com/...', techs: ["Node.js", "..."], likes: 0 }; Certifique-se que o ID seja um UUID, e de sempre iniciar os likes como 0.
+
+### **`GET`** `/repositories`
+
+Rota que lista todos os repositórios.
+
+### **`PUT`** `/repositories/:id`
+
+A rota deve alterar apenas o title, a url e as techs do repositório que possua o id igual ao id presente nos parâmetros da rota.
+
+### **`DELETE`** `/repositories/:id`
+
+A rota deve deletar o repositório com o id presente nos parâmetros da rota.
+
+### **`POST`** `/repositories/:id/like`
+
+A rota deve aumentar o número de likes do repositório específico escolhido através do id presente nos parâmetros da rota, a cada chamada dessa rota, o número de likes deve ser aumentado em 1.
--- src/app.js
+++ src/app.js
@@ -1,33 +1,94 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid, isUuid } = require("uuidv4");
 
-// const { uuid } = require("uuidv4");
-
-const app = express();
-
-app.use(express.json());
-app.use(cors());
+const app = express().use(express.json()).use(cors());
 
 const repositories = [];
 
+const validateRepositoryID = (request, response, next) => {
+	const { id } = request.params;
+
+	if (!isUuid(id))
+		return response.status(400).json({ error: "Invalid repository ID" });
+
+	next();
+};
+
 app.get("/repositories", (request, response) => {
-  // TODO
+	if (repositories.length === 0)
+		return response.status(200).json({ error: "Repository is empty" });
+
+	return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+	const { title, url, techs } = request.body;
+
+	const repository = {
+		id: uuid(),
+		title,
+		url,
+		techs,
+		likes: 0,
+	};
+
+	repositories.push(repository);
+
+	return response.status(201).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateRepositoryID, (request, response) => {
+	const { id } = request.params;
+	const { title, url, techs } = request.body;
+
+	const repositoryIndex = repositories.findIndex((repository) => repository.id === id);
+
+	if (repositoryIndex < 0)
+		return response.status(404).json({ error: "Repository not found" });
+
+	const repository = {
+		id,
+		title,
+		url,
+		techs,
+		likes: repositories[repositoryIndex].likes,
+	};
+
+	repositories[repositoryIndex] = repository;
+
+	return response.status(200).json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateRepositoryID, (request, response) => {
+	const { id } = request.params;
+
+	const repositoryIndex = repositories.findIndex((repository) => repository.id === id);
+
+	if (repositoryIndex < 0)
+		return response.status(404).json({ error: "Repository not found" });
+
+	repositories.splice(repositoryIndex, 1);
+
+	return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateRepositoryID, (request, response) => {
+	const { id } = request.params;
+
+	const repositoryIndex = repositories.findIndex((repository) => repository.id === id);
+
+	if (repositoryIndex < 0)
+		return response.status(404).json({ error: "Repository not found" });
+
+	repositories[repositoryIndex].likes += 1;
+
+	const repositoryLikes = {
+		id,
+		likes: repositories[repositoryIndex].likes
+	}
+
+	return response.status(200).json(repositoryLikes);
 });
 
 module.exports = app;
